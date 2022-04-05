a57114a7ad57cf457f8a99b521d93ec916c727e0
mathangwane <mathangwane@gmail.com>
2022-04-05 06:52:41 +0200

    05 April 2022

--- eswatini_covid19_infections.py
+++ eswatini_covid19_infections.py
@@ -1,13 +1,14 @@
 import pandas as pd 
 from matplotlib import pyplot as plt
 
-plt.xkcd()
+#plt.xkcd()
 plt.style.use('seaborn')
 
 data = pd.read_csv('data/wave1.csv')
 data2 = pd.read_csv('data/wave2.csv')
 data3 = pd.read_csv('data/wave3.csv')
 data4 = pd.read_csv('data/wave4.csv')
+data5 = pd.read_csv('data/wave5.csv')
 
 Infection_Day = data['Day']
 Infection_Number = data['Infections']
@@ -21,10 +22,14 @@ Infection_Number3 = data3['Infections']
 Infection_Day4 = data4['Day']
 Infection_Number4 = data4['Infections']
 
+Infection_Day5 = data5['Day']
+Infection_Number5 = data5['Infections']
+
 plt.plot(Infection_Day, Infection_Number, marker=None, linestyle='solid')
 plt.plot(Infection_Day2, Infection_Number2, marker=None, linestyle='solid')
 plt.plot(Infection_Day3, Infection_Number3, marker=None, linestyle='solid')
 plt.plot(Infection_Day4, Infection_Number4, marker=None, linestyle='solid')
+plt.plot(Infection_Day5, Infection_Number5, marker=None, linestyle='solid')
 
 plt.title('Covid Waves in Eswatini \n (Data Source: Government of Eswatini)')
 plt.xlabel('Number of Days Since Start of Wave \n Graphic by Mathangwane (https://github.com/mathangwane/)')
@@ -32,6 +37,6 @@ plt.ylabel('Confirmed Infections (7-Day Moving Average)')
 
 plt.tight_layout()
 
-plt.legend(["First Wave", "Second Wave", "Third Wave", "Fourth Wave"])
+plt.legend(["First Wave", "Second Wave", "Third Wave", "Fourth Wave", "Fifth Wave"])
 
 plt.show()
